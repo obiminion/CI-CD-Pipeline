9e9957c22b30ad1e45768495e2930b8c737fbf26
obiminion <49394997+obiminion@users.noreply.github.com>
2023-09-17 10:41:37 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "DO_TOKEN" {
-  description = "dop_v1_a2c67cc4520116cf0aa058efe855c0d842f5f9fa4aa03bf50e4ae4bb5ac39ea8"
+  description = "DigitalOcean API Token"
   type        = string
 }
 
 variable "SSH_KEY" {
-  description = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDDCj7RE7BrIMOuirfgC55Vgl6luf0zhL1Q4/60B9A6IiTSKVO4Ek5KD1MDmnosze/ZxxS3Rb9qyatmkcbF5KF3WNErMGNn6oFaMftcaPNtKwmtfAKhS81w89R4jVJHuBPuvgUKhTI9JNdj+8EaCNr+vXPQorHYYr4hHbxCNXyoQC7BYlflLB0fPcR8oHOknjk1Mg3PWkrgNMIyreEms7ixMx8+dQEvY3cUovKMh6u8Qhasf14C/a2FW5+qY1dNjx+6Ai6sxVsaLDm4MIRP2ZsSaVTKH5d0v5scTA0U3pfxZ+gbiEqehnkzT12ZHmJPEi2sT93sZ5QP8TRs5iyuRKBD rsa-key-20230916"
+  description = "SSH Key for Droplet"
   type        = string
 }
